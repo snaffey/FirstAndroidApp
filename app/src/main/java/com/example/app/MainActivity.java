000bb1936e4274557b81d90a20221670c9ac3a69
Tiago <ggtiago0santos@gmail.com>
2023-04-21 09:52:23 +0100

    a

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.TextView;
+
+import android.widget.EditText;
+
 public class MainActivity extends AppCompatActivity {
 
+    TextView viewRes;
+    EditText n1, n2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_5);
+        setContentView(R.layout.mainsoma);
+        int a;
+        viewRes = (TextView) findViewById(R.id.viewRes);
+        n1 = (EditText) findViewById(R.id.n1);
+        n2 = (EditText) findViewById(R.id.n2);
+        Button btnSoma = (Button) findViewById(R.id.btnSomar);
+        // setText() -> alterar via java o conteudo da view
+        viewRes.setText("Alterado via java");
+    }
+    public void alterarTxt(){
+        viewRes.setText("Alterado via java");
     }
 }
